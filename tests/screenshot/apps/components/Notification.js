05ff109585e7a3dc7def8d5299d8690bec8ee223
Lucie Roy <lucie.roy@lge.com>
2020-01-23 15:27:04 -0800

    add tests to Notification

--- tests/screenshot/apps/components/Notification.js
+++ tests/screenshot/apps/components/Notification.js
@@ -1,7 +1,36 @@
 import Notification from '../../../../Notification';
+import Button from '@enact/moonstone/Button';
 import React from 'react';
 
 const NotificationTests = [
-	<Notification />
+	// Notification Displays Over Background - [GT-21554]
+	<Notification open>Notification has content in it and can be very useful for organizing information for the user.</Notification>,
+	// end of [GT-21554]
+	<Notification open>-Lorem</Notification>,
+	{
+		textSize: 'large',
+		component: <Notification open>-Lorem</Notification>
+	},
+	// <Notification open scrimType="transparent">-Lorem</Notification>,
+	// <Notification open scrimType="translucent">-Lorem</Notification>,
+	// <Notification><buttons><Button>Hello</Button></buttons></Notification>,
+	// <Notification><>Hello<buttons><Button>Hello</Button></buttons></></Notification>
+	// *************************************************************
+	// locale = 'ar-SA'
+	// *************************************************************
+	{
+		locale: 'ar-SA',
+		component: <Notification open>Notification has content in it and can be very useful for organizing information for the user.</Notification>
+	},
+	{
+		locale: 'ar-SA',
+		component: <Notification open>-Lorem</Notification>
+	},
+	{
+		locale: 'ar-SA',
+		textSize: 'large',
+		component: <Notification open>-Lorem</Notification>
+	}
+
 ];
 export default NotificationTests;
