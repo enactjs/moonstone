1bdca66f3ec86be3c8f152100a816d7b87a7f525
Lucie Roy <lucie.roy@lge.com>
2020-01-30 10:54:26 -0800

    added tests to Notifications

--- tests/screenshot/apps/components/Notification.js
+++ tests/screenshot/apps/components/Notification.js
@@ -3,14 +3,31 @@ import Button from '@enact/moonstone/Button';
 import React from 'react';
 
 const NotificationTests = [
+	// withLongButtonsAndSinhala: Text does not overlap in Notification Popup [GT-27998}
+	// Left the Jira test as manual since we cannot use the same string length with SST.
+	// Below we are using a much shorter string.
+	{
+		locale: 'si-LK',
+		component: <Notification open><><div>ේවය නඩත්තු කිරීම හෝ වැඩි දියුණු කිරීම සඳහා කලා ගැලරිය සේවයට එකතු කිරීම, නවීකරණය කිරීම, පිවිසීම</div><buttons><Button>OK</Button><Button>Nevermind</Button></buttons></></Notification>
+	},
+	// end of [GT-27998}
+
 	// Notification Displays Over Background - [GT-21554]
-	<Notification open>Notification has content in it and can be very useful for organizing information for the user.</Notification>,
+	<Notification open><><div>Notification has content in it and can be very useful for organizing information for the user.</div><buttons><Button>OK</Button><Button>Nevermind</Button></buttons></></Notification>,
 	// end of [GT-21554]
-	<Notification open>-Lorem</Notification>,
+
+	// Width Retains in Large Text inside Notifications [GT-24086]
+	// Step 4: Text (within the buttons) and buttons size expand. Buttons display aligned with each other.
 	{
 		textSize: 'large',
-		component: <Notification open>-Lorem</Notification>
+		component: <Notification open><><div>Notification has content in it and can be very useful for organizing information for the user.</div><buttons><Button>OK</Button><Button>Nevermind</Button></buttons></></Notification>
 	},
+	// end of [GT-24086]
+
+	// Width adjusts to the width of the Message - [GT-27849]
+	<Notification open><><div>Hello</div><buttons><Button>OK</Button><Button>Nevermind</Button></buttons></></Notification>,
+	// end of [GT-27849]
+
 	{
 		wrapper: 'full',
 		component: <Notification open scrimType="transparent">-Lorem</Notification>
@@ -19,24 +36,49 @@ const NotificationTests = [
 		wrapper: 'full',
 		component: <Notification open scrimType="translucent">-Lorem</Notification>
 	},
-	<Notification open><buttons><Button>Hello</Button></buttons></Notification>,
-	<Notification open><>Hello<buttons><Button>Hello</Button></buttons></></Notification>,
+
+	<Notification open>-Lorem</Notification>,
+
 	// *************************************************************
 	// locale = 'ar-SA'
 	// *************************************************************
+	// Alignment in RTL [GT-23434]
+	// The text is right aligned. Buttons flip position.
 	{
 		locale: 'ar-SA',
-		component: <Notification open>Notification has content in it and can be very useful for organizing information for the user.</Notification>
+		component:<Notification open><><div>Notification has content in it and can be very useful for organizing information for the user.</div><buttons><Button>OK</Button><Button>Nevermind</Button></buttons></></Notification>
 	},
-	{
-		locale: 'ar-SA',
-		component: <Notification open>-Lorem</Notification>
-	},
+	// end of [GT-23434]
+
+	// Width Retains in Large Text inside Notifications [GT-24086]
+	// Step 4: Text (within the buttons) and buttons size expand. Buttons display aligned with each other.
 	{
 		locale: 'ar-SA',
 		textSize: 'large',
+		component: <Notification open><><div>Notification has content in it and can be very useful.</div><buttons><Button>OK</Button><Button>Nevermind</Button></buttons></></Notification>
+	},
+	// end of [GT-24086]
+
+	// Width adjusts to the width of the Message - [GT-27849]
+	{
+		locale: 'ar-SA',
+		component: <Notification open><><div>Hello</div><buttons><Button>OK</Button><Button>Nevermind</Button></buttons></></Notification>
+	},
+	// end of [GT-27849]
+
+	{
+		locale: 'ar-SA',
+		wrapper: 'full',
+		component: <Notification open scrimType="transparent">-Lorem</Notification>
+	},
+	{
+		locale: 'ar-SA',
+		wrapper: 'full',
+		component: <Notification open scrimType="translucent">-Lorem</Notification>
+	},
+	{
+		locale: 'ar-SA',
 		component: <Notification open>-Lorem</Notification>
 	}
-
 ];
 export default NotificationTests;
