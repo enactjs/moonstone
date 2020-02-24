e62716899b1cff253d88ff74e0df1de2748f9a45
YunBum Sung <yb.sung@lge.com>
2020-02-24 13:03:35 +0900

    Fix wrong component path
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

--- samples/sampler/stories/qa/components/PickerAddRemove/PickerAddRemove.js
+++ samples/sampler/stories/qa/components/PickerAddRemove/PickerAddRemove.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Button from '@enact/moonstone/../../Button';
-import Input from '@enact/moonstone/../../Input';
-import Picker from '@enact/moonstone/../../Picker';
+import Button from '@enact/moonstone/Button';
+import Input from '@enact/moonstone/Input';
+import Picker from '@enact/moonstone/Picker';
 
 class PickerAddRemove extends React.Component {
 	static displayName = 'PickerAddRemove'
