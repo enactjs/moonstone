2f5a23245681f555a6f4b795d4603f4e8e84cd9f
Roy Sutton <roy.sutton@lge.com>
2020-03-03 15:20:37 -0500

    Port VirtualList utils
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- tests/ui/specs/VirtualList/VirtualList-utils.js
+++ tests/ui/specs/VirtualList/VirtualList-utils.js
@@ -1,6 +1,10 @@
 /* global document */
+function focusedElement () {
+	return browser.execute(function () { return document.activeElement.id; });
+}
+
 function expectFocusedItem (itemNum, comment = 'focused item') {
-	const focusedId = browser.execute(function () { return document.activeElement.id; });
+	const focusedId = focusedElement();
 	expect(focusedId, comment).to.equal(`item${itemNum}`);
 }
 
@@ -8,5 +12,33 @@ function expectNoFocusedItem () {
 	expect(browser.execute(function () { return document.activeElement === document.body; })).to.be.true();
 }
 
+function waitUntilFocused (itemNum) {
+	const target = `item${itemNum}`;
+	browser.waitUntil(function () {
+		const focusedId = focusedElement();
+		return target === focusedId;
+	}, 1500, `timed out waiting to focus index ${itemNum}`);
+}
+
+function isScrolling () {
+	return $('#scrolling').getText() === 'Scrolling';
+}
+
+function isNotScrolling () {
+	return $('#scrolling').getText() === 'Not Scrolling';
+}
+
+/**
+ * Waits for scrolling to start, then stop
+ *
+ * @param {Number} [timeout=3000]
+ */
+function waitForScrollStop (timeout = 3000) {
+	browser.waitUntil(isScrolling, timeout);
+	browser.waitUntil(isNotScrolling, timeout);
+}
+
 exports.expectFocusedItem = expectFocusedItem;
 exports.expectNoFocusedItem = expectNoFocusedItem;
+exports.waitForScrollStop = waitForScrollStop;
+exports.waitUntilFocused = waitUntilFocused;
