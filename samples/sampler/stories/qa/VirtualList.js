dd5a39c7ff6b50d4aba766d288b8140ae6324d61
Seungcheon Baek <sc.baek@lge.com>
2020-10-08 13:10:56 +0900

    Revert "Fix VirtualList qa sampler"
    This reverts commit 315cc40af5b8165a142eda5e4d125e7471be0381.

--- samples/sampler/stories/qa/VirtualList.js
+++ samples/sampler/stories/qa/VirtualList.js
@@ -342,7 +342,6 @@ storiesOf('VirtualList', module)
 					focusableScrollbar={boolean('focusableScrollbar', Config)}
 					itemRenderer={renderItem(ContainerItemWithControls, ri.scale(number('itemSize', Config, 78)), true)}
 					itemSize={ri.scale(number('itemSize', Config, 78))}
-					spotlightId="virtual-list"
 					wrap={wrapOption[select('wrap', ['false', 'true', '"noAnimation"'], Config)]}
 				/>
 			);
