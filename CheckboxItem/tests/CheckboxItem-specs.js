090851205e5e23b6389268707c5cd4c31345cb9e
daniel-stoian-lgp <63335068+daniel-stoian-lgp@users.noreply.github.com>
2021-07-26 15:06:43 +0300

    WRN-3499: Added unit tests for Checkbox and CheckboxItem (#63)
    * added unit tests for Checkbox and CheckboxItem
    
    * fixes

---
+++ CheckboxItem/tests/CheckboxItem-specs.js
@@ -0,0 +1,50 @@
+import '@testing-library/jest-dom';
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import CheckboxItem, {CheckboxItemBase} from '../CheckboxItem';
+
+describe('CheckboxItem Specs', () => {
+	test('should support a custom icon', () => {
+		render(<CheckboxItemBase data-testid="checkbox" icon="music">Hello CheckboxItem</CheckboxItemBase>);
+
+		const checkboxItem = screen.getByRole('checkbox');
+
+		const actual = checkboxItem.textContent.codePointAt();
+		const expected = 983138; // decimal converted charCode of Unicode 'music' character
+
+		expect(actual).toBe(expected);
+	});
+
+	test('should have correct text', () => {
+		render(<CheckboxItemBase>Hello CheckboxItem</CheckboxItemBase>);
+
+		const element = screen.getByText(/Hello CheckboxItem/i);
+
+		expect(element).toBeInTheDocument();
+	});
+
+	test('should check with click', () => {
+		render(<CheckboxItem>Hello CheckboxItem</CheckboxItem>);
+
+		const checkboxItem = screen.getByRole('checkbox');
+		const expected = 'true';
+
+		userEvent.click(checkboxItem);
+
+		expect(checkboxItem).toHaveAttribute('aria-checked', expected);
+
+	});
+
+	test('should uncheck with 2 click', () => {
+		render(<CheckboxItem>Hello CheckboxItem</CheckboxItem>);
+
+		const checkboxItem = screen.getByRole('checkbox');
+		const expected = 'false';
+
+		userEvent.click(checkboxItem);
+		userEvent.click(checkboxItem);
+
+		expect(checkboxItem).toHaveAttribute('aria-checked', expected);
+	});
+});
