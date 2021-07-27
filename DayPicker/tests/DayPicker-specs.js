53796a58d1a5c82910ddec72a8bbb3c9fb9cbf9a
Stanca Pop <stanca.pop@lgepartner.com>
2021-07-27 15:51:14 +0300

    migrated unit tests to react testing library

--- DayPicker/tests/DayPicker-specs.js
+++ DayPicker/tests/DayPicker-specs.js
@@ -1,4 +1,6 @@
-import {mount} from 'enzyme';
+import '@testing-library/jest-dom';
+import {render, screen} from '@testing-library/react';
+
 import DayPicker from '../DayPicker';
 
 describe('DayPicker', () => {
@@ -6,73 +8,72 @@ describe('DayPicker', () => {
 		test(
 			'should use title, selected long string when day is single selected',
 			() => {
-				const subject = mount(
+				render(
 					<DayPicker title="Day Picker" selected={0} />
 				);
 
 				const expected = 'Day Picker Sunday';
-				const actual = subject.find('DayPicker').prop('aria-label');
+				const actual = screen.getByRole('group');
 
-				expect(actual).toBe(expected);
+				expect(actual).toHaveAttribute('aria-label', expected);
 			}
 		);
 
 		test(
 			'should use title, selected long string when day is multi selected',
 			() => {
-				const subject = mount(
+				render(
 					<DayPicker title="Day Picker" selected={[0, 1]} />
 				);
 
 				const expected = 'Day Picker Sunday, Monday';
-				const actual = subject.find('DayPicker').prop('aria-label');
+				const actual = screen.getByRole('group');
 
-				expect(actual).toBe(expected);
+				expect(actual).toHaveAttribute('aria-label', expected);
 			}
 		);
 
 		test('should be null when day is not selected', () => {
-			const subject = mount(
+			render(
 				<DayPicker title="Day Picker" />
 			);
 
-			const expected = undefined; // eslint-disable-line no-undefined
-			const actual = subject.find('DayPicker').prop('aria-label');
+			const actual = screen.getByRole('group');
 
-			expect(actual).toBe(expected);
+			expect(actual).not.toHaveAttribute('aria-label');
 		});
 
-		test('should be null when every day is selected', () => {
-			const subject = mount(
+		test('should use title, everyDayText when every day is selected', () => {
+			render(
 				<DayPicker title="Day Picker" everyDayText="every" selected={[0, 1, 2, 3, 4, 5, 6]} />
 			);
 
 			const expected = 'Day Picker every';
-			const actual = subject.find('DayPicker').prop('aria-label');
+			const actual = screen.getByRole('group');
 
-			expect(actual).toBe(expected);
+			expect(actual).toHaveAttribute('aria-label', expected);
 		});
 
-		test('should be null when every weekday is selected', () => {
-			const subject = mount(
+		test('should use title, everyWeekdayText when every weekday is selected', () => {
+			render(
 				<DayPicker title="Day Picker" everyWeekdayText="weekday" selected={[1, 2, 3, 4, 5]} />
 			);
 
 			const expected = 'Day Picker weekday';
-			const actual = subject.find('DayPicker').prop('aria-label');
+			const actual = screen.getByRole('group');
 
-			expect(actual).toBe(expected);
+			expect(actual).toHaveAttribute('aria-label', expected);
 		});
 
-		test('should be null when every weekend is selected', () => {
-			const subject = mount(
+		test('should use title, everyWeekendText when every weekend is selected', () => {
+			render(
 				<DayPicker title="Day Picker" everyWeekendText="weekend" selected={[0, 6]} />
 			);
 
 			const expected = 'Day Picker weekend';
-			const actual = subject.find('DayPicker').prop('aria-label');
+			const actual = screen.getByRole('group');
 
-			expect(actual).toBe(expected);
+			expect(actual).toHaveAttribute('aria-label', expected);
 		});
 	});
 });
