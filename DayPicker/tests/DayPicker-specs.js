2d9b4670ef9cca9dc716bb6bbcb18cdbc773e218
Stanca Pop <stanca.pop@lgepartner.com>
2021-07-28 12:35:06 +0300

    review fixes

--- DayPicker/tests/DayPicker-specs.js
+++ DayPicker/tests/DayPicker-specs.js
@@ -5,38 +5,26 @@ import DayPicker from '../DayPicker';
 
 describe('DayPicker', () => {
 	describe('#aria-label', () => {
-		test(
-			'should use title, selected long string when day is single selected',
-			() => {
-				render(
-					<DayPicker title="Day Picker" selected={0} />
-				);
+		test('should use title, selected long string when day is single selected', () => {
+			render(<DayPicker title="Day Picker" selected={0} />);
 
-				const expected = 'Day Picker Sunday';
-				const actual = screen.getByRole('group');
+			const expected = 'Day Picker Sunday';
+			const actual = screen.getByRole('group');
 
-				expect(actual).toHaveAttribute('aria-label', expected);
-			}
-		);
+			expect(actual).toHaveAttribute('aria-label', expected);
+		});
 
-		test(
-			'should use title, selected long string when day is multi selected',
-			() => {
-				render(
-					<DayPicker title="Day Picker" selected={[0, 1]} />
-				);
+		test('should use title, selected long string when day is multi selected', () => {
+			render(<DayPicker title="Day Picker" selected={[0, 1]} />);
 
-				const expected = 'Day Picker Sunday, Monday';
-				const actual = screen.getByRole('group');
+			const expected = 'Day Picker Sunday, Monday';
+			const actual = screen.getByRole('group');
 
-				expect(actual).toHaveAttribute('aria-label', expected);
-			}
-		);
+			expect(actual).toHaveAttribute('aria-label', expected);
+		});
 
 		test('should be null when day is not selected', () => {
-			render(
-				<DayPicker title="Day Picker" />
-			);
+			render(<DayPicker title="Day Picker" />);
 
 			const actual = screen.getByRole('group');
 
@@ -44,9 +32,7 @@ describe('DayPicker', () => {
 		});
 
 		test('should use title, everyDayText when every day is selected', () => {
-			render(
-				<DayPicker title="Day Picker" everyDayText="every" selected={[0, 1, 2, 3, 4, 5, 6]} />
-			);
+			render(<DayPicker title="Day Picker" everyDayText="every" selected={[0, 1, 2, 3, 4, 5, 6]} />);
 
 			const expected = 'Day Picker every';
 			const actual = screen.getByRole('group');
@@ -55,9 +41,7 @@ describe('DayPicker', () => {
 		});
 
 		test('should use title, everyWeekdayText when every weekday is selected', () => {
-			render(
-				<DayPicker title="Day Picker" everyWeekdayText="weekday" selected={[1, 2, 3, 4, 5]} />
-			);
+			render(<DayPicker title="Day Picker" everyWeekdayText="weekday" selected={[1, 2, 3, 4, 5]} />);
 
 			const expected = 'Day Picker weekday';
 			const actual = screen.getByRole('group');
@@ -66,9 +50,7 @@ describe('DayPicker', () => {
 		});
 
 		test('should use title, everyWeekendText when every weekend is selected', () => {
-			render(
-				<DayPicker title="Day Picker" everyWeekendText="weekend" selected={[0, 6]} />
-			);
+			render(<DayPicker title="Day Picker" everyWeekendText="weekend" selected={[0, 6]} />);
 
 			const expected = 'Day Picker weekend';
 			const actual = screen.getByRole('group');
