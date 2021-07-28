816d73a3e06c63c781346422621180b1d8b40465
Alex Morariu <alexandru.morariu@lgepartner.com>
2021-07-28 12:16:29 +0300

    Migrated tests from Enzyme to Testing-Library.

--- DaySelector/tests/DaySelector-specs.js
+++ DaySelector/tests/DaySelector-specs.js
@@ -1,157 +1,133 @@
-import {mount} from 'enzyme';
+import '@testing-library/jest-dom';
+import {fireEvent, render, screen} from '@testing-library/react';
 
 import DaySelector from '../DaySelector';
 
 const tap = (node) => {
-	node.simulate('mousedown');
-	node.simulate('mouseup');
-	node.simulate('click');
+	fireEvent.mouseDown(node);
+	fireEvent.mouseUp(node);
+	fireEvent.click(node);
 };
 
 describe('DaySelector', () => {
 
-	test(
-		'should set selected prop to true for the item that is selected by default',
-		() => {
-			const subject = mount(
-				<DaySelector defaultSelected={0} />
-			);
+	test('should set selected prop to true for the item that is selected by default', () => {
+		render(<DaySelector defaultSelected={0} />);
+		const daySelector = screen.getAllByRole('checkbox')[0].children.item(0).children.item(0);
 
-			const item = subject.find('DaySelectorItem').first();
+		const expectedAttribute = 'selected';
 
-			const expected = true;
-			const actual = item.prop('selected');
-
-			expect(actual).toBe(expected);
-		}
-	);
+		expect(daySelector).toHaveClass(expectedAttribute);
+	});
 
 	test('should fire onSelect when a day is selected', () => {
 		const handleSelect = jest.fn();
-		const subject = mount(
-			<DaySelector onSelect={handleSelect} />
-		);
+		render(<DaySelector onSelect={handleSelect} />);
+		const item = screen.getAllByRole('checkbox')[0];
 
-		const item = subject.find('DaySelectorItem').first();
 		tap(item);
 
-		const expected = 1;
-		const actual = handleSelect.mock.calls.length;
+		expect(handleSelect).toHaveBeenCalled();
+	});
+
+	test('should fire onSelect with the correct content when a day is selected', () => {
+		const handleSelect = jest.fn();
+		render(
+			<DaySelector onSelect={handleSelect} />
+		);
+		const item = screen.getAllByRole('checkbox')[6];
+
+		tap(item);
+
+		const expected = 'Sat';
+		const actual = handleSelect.mock.calls[0][0].content;
 
 		expect(actual).toBe(expected);
 	});
 
-	test(
-		'should fire onSelect with the correct content when a day is selected',
-		() => {
-			const handleSelect = jest.fn();
-			const content = 'Sat';
-			const subject = mount(
-				<DaySelector onSelect={handleSelect} />
-			);
+	test('should use the full string format when dayNameLength is `full`', () => {
+		const handleSelect = jest.fn();
+		render(
+			<DaySelector dayNameLength="full" onSelect={handleSelect} />
+		);
+		const item = screen.getAllByRole('checkbox')[6];
 
-			const item = subject.find('DaySelectorItem').last();
-			tap(item);
+		tap(item);
 
-			const expected = content;
-			const actual = handleSelect.mock.calls[0][0].content;
+		const expected = 'Saturday';
+		const actual = handleSelect.mock.calls[0][0].content;
 
-			expect(actual).toBe(expected);
-		}
-	);
+		expect(actual).toBe(expected);
+	});
 
-	test(
-		'should use the full string format when dayNameLength is `full`',
-		() => {
-			const handleSelect = jest.fn();
-			const content = 'Saturday';
-			const subject = mount(
-				<DaySelector dayNameLength="full" onSelect={handleSelect} />
-			);
+	test('should set selected content as Every Day when every day is selected', () => {
+		const handleSelect = jest.fn();
+		render(
+			<DaySelector
+				defaultSelected={[0, 1, 2, 3, 4, 5]}
+				onSelect={handleSelect}
+			/>
+		);
+		const item = screen.getAllByRole('checkbox')[6];
 
-			const item = subject.find('DaySelectorItem').last();
-			tap(item);
+		tap(item);
 
-			const expected = content;
-			const actual = handleSelect.mock.calls[0][0].content;
+		const expected = 'Every Day';
+		const actual = handleSelect.mock.calls[0][0].content;
 
-			expect(actual).toBe(expected);
-		}
-	);
+		expect(actual).toBe(expected);
+	});
 
-	test(
-		'should set selected content as Every day when every day is selected',
-		() => {
-			const handleSelect = jest.fn();
-			const content = 'Every Day';
-			const subject = mount(
-				<DaySelector defaultSelected={[0, 1, 2, 3, 4, 5]} onSelect={handleSelect} />
-			);
+	test('should set selected content as Every Weekday when every weekday is selected', () => {
+		const handleSelect = jest.fn();
+		render(
+			<DaySelector
+				defaultSelected={[1, 2, 3, 4]}
+				onSelect={handleSelect}
+			/>
+		);
+		const item = screen.getAllByRole('checkbox')[5];
 
-			const item = subject.find('DaySelectorItem').last();
-			tap(item);
+		tap(item);
 
-			const expected = content;
-			const actual = handleSelect.mock.calls[0][0].content;
+		const expected = 'Every Weekday';
+		const actual = handleSelect.mock.calls[0][0].content;
 
-			expect(actual).toBe(expected);
-		}
-	);
+		expect(actual).toBe(expected);
+	});
 
-	test(
-		'should set selected content as Every weekday when every weekday is selected',
-		() => {
-			const handleSelect = jest.fn();
-			const content = 'Every Weekday';
-			const subject = mount(
-				<DaySelector defaultSelected={[0, 1, 2, 3, 4, 5]} onSelect={handleSelect} />
-			);
+	test('should set selected content as Every Weekend when every weekend is selected', () => {
+		const handleSelect = jest.fn();
+		render(
+			<DaySelector defaultSelected={[0]} onSelect={handleSelect} />
+		);
+		const item = screen.getAllByRole('checkbox')[6];
 
-			const item = subject.find('DaySelectorItem').first();
-			tap(item);
+		tap(item);
 
-			const expected = content;
-			const actual = handleSelect.mock.calls[0][0].content;
+		const expected = 'Every Weekend';
+		const actual = handleSelect.mock.calls[0][0].content;
 
-			expect(actual).toBe(expected);
-		}
-	);
+		expect(actual).toBe(expected);
+	});
 
-	test(
-		'should set selected content as Every weekend when every weekend is selected',
-		() => {
-			const handleSelect = jest.fn();
-			const content = 'Every Weekend';
-			const subject = mount(
-				<DaySelector defaultSelected={[0]} onSelect={handleSelect} />
-			);
+	test('should render updated day name length', () => {
+		const {rerender} = render(<DaySelector dayNameLength={'full'} />);
 
-			const item = subject.find('DaySelectorItem').last();
-			tap(item);
+		rerender(<DaySelector dayNameLength={'short'} />);
 
-			const expected = content;
-			const actual = handleSelect.mock.calls[0][0].content;
+		const expected = '✓S✓M✓T✓W✓T✓F✓S';
+		const actual = screen.getByRole('group').textContent;
 
-			expect(actual).toBe(expected);
-		}
-	);
+		expect(actual).toBe(expected);
+	});
 
-	test(
-		'should render updated day name length.',
-		() => {
-			const subject = mount(
-				<DaySelector dayNameLength={'full'} />
-			);
+	test('should have prop disabled when `disable` is true.', () => {
+		render(<DaySelector disabled />);
+		const daySelector = screen.getByRole('group');
 
-			subject.setProps({
-				dayNameLength: 'short'
-			});
-			subject.update();
+		const expectedAttribute = 'disabled';
 
-			const expected = '✓S✓M✓T✓W✓T✓F✓S';
-			const actual = subject.text();
-
-			expect(actual).toBe(expected);
-		}
-	);
+		expect(daySelector).toHaveAttribute(expectedAttribute);
+	});
 });
