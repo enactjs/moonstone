84afc44a732848697f7897d727fafaf675cfedd8
Stanca Pop <stanca.pop@lgepartner.com>
2021-07-28 16:13:45 +0300

    minor fix

--- SwitchItem/tests/SwitchItem-specs.js
+++ SwitchItem/tests/SwitchItem-specs.js
@@ -2,8 +2,7 @@ import '@testing-library/jest-dom';
 import {render, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import SwitchItem from '../SwitchItem';
-import {SwitchItemBase} from '../../../sandstone/SwitchItem';
+import SwitchItem, {SwitchItemBase} from '../SwitchItem';
 
 describe('SwitchItem Specs', () => {
 	test('should contain a Switch', () => {
