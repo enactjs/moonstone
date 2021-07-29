d8b7cf538ded37a2291f35de0aca5c1a4f70eddf
Stanca <63341832+stanca-pop-lgp@users.noreply.github.com>
2021-07-29 14:26:20 +0300

    WRN-3835: Migrate Item, LabeledItem and SelectableItem unit tests to Testing Library (#71)
    * migrated unit tests to react testing library
    
    * review fixes

---
+++ Item/tests/Item-specs.js
@@ -0,0 +1,33 @@
+import '@testing-library/jest-dom';
+import {render, screen} from '@testing-library/react';
+
+import {ItemBase} from '../Item';
+
+describe('Item Specs', () => {
+	test('should support adding text as a child', () => {
+		const expected = 'Hello Item';
+		render(<ItemBase>{expected}</ItemBase>);
+
+		const actual = screen.getByText('Hello Item');
+
+		expect(actual).toHaveTextContent(expected);
+	});
+
+	test('should apply `inline` class when inline', () => {
+		render(<ItemBase inline>Hello Item</ItemBase>);
+
+		const expected = 'inline';
+		const actual = screen.getByText('Hello Item');
+
+		expect(actual).toHaveClass(expected);
+	});
+
+	test('should have `aria-disabled=true` attribute when disabled', () => {
+		render(<ItemBase disabled>Hello Item</ItemBase>);
+
+		const expected = 'true';
+		const actual = screen.getByText('Hello Item');
+
+		expect(actual).toHaveAttribute('aria-disabled', expected);
+	});
+});
