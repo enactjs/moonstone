042866ff448565ded60965f77cceb770fe675626
Stanca <63341832+stanca-pop-lgp@users.noreply.github.com>
2021-07-29 14:33:03 +0300

    WRN-3811: Migrate LabeledIconButton, Switch and SwitchItem unit tests to Testing Library (#70)
    * migrated tests to react testing library
    
    * fixed lint warnings
    
    * minor fix
    
    * fixed tests
    
    * review fix
    
    * added extra tests
    
    * added extra tests

--- LabeledIconButton/tests/LabeledIconButton-specs.js
+++ LabeledIconButton/tests/LabeledIconButton-specs.js
@@ -1,59 +1,72 @@
-import {mount} from 'enzyme';
+import '@testing-library/jest-dom';
+import {render, screen} from '@testing-library/react';
+
 import LabeledIconButton from '../LabeledIconButton';
 
 describe('LabeledIconButton Voice Control Specs', () => {
-	test(
-		'should set "data-webos-voice-disabled" to IconButton',
-		() => {
-			const labeledIconButton = mount(
-				<LabeledIconButton data-webos-voice-disabled>star</LabeledIconButton>
-			);
+	test('should not have `selected` className by default', () => {
+		render(<LabeledIconButton>star</LabeledIconButton>);
 
-			const expected = true;
-			const actual = labeledIconButton.find('[role="button"]').prop('data-webos-voice-disabled');
-			expect(actual).toBe(expected);
-		}
-	);
+		const expected = 'selected';
+		const actual = screen.getByRole('button');
 
-	test(
-		'should set "data-webos-voice-group-label" to IconButton',
-		() => {
-			const voiceGroupLabel = 'voice group label';
-			const labeledIconButton = mount(
-				<LabeledIconButton data-webos-voice-group-label={voiceGroupLabel}>star</LabeledIconButton>
-			);
+		expect(actual).not.toHaveClass(expected);
+	});
 
-			const expected = voiceGroupLabel;
-			const actual = labeledIconButton.find('[role="button"]').prop('data-webos-voice-group-label');
-			expect(actual).toBe(expected);
-		}
-	);
+	test('should have `selected` className when selected is true', () => {
+		render(<LabeledIconButton selected>star</LabeledIconButton>);
 
-	test(
-		'should set "data-webos-voice-label" to IconButton',
-		() => {
-			const voiceLabel = 'voice label';
-			const labeledIconButton = mount(
-				<LabeledIconButton data-webos-voice-label={voiceLabel}>star</LabeledIconButton>
-			);
+		const expected = 'selected';
+		const actual = screen.getByRole('button');
 
-			const expected = voiceLabel;
-			const actual = labeledIconButton.find('[role="button"]').prop('data-webos-voice-label');
-			expect(actual).toBe(expected);
-		}
-	);
+		expect(actual).toHaveClass(expected);
+	});
 
-	test(
-		'should set "data-webos-voice-intent" to IconButton',
-		() => {
-			const voiceIntent = 'Select PlayContent';
-			const labeledIconButton = mount(
-				<LabeledIconButton data-webos-voice-intent={voiceIntent}>star</LabeledIconButton>
-			);
+	test('should have `flipHorizontal` className when "flip" is set to "horizontal"', () => {
+		render(<LabeledIconButton flip="horizontal">star</LabeledIconButton>);
 
-			const expected = voiceIntent;
-			const actual = labeledIconButton.find('[role="button"]').prop('data-webos-voice-intent');
-			expect(actual).toBe(expected);
-		}
-	);
+		const expected = 'flipHorizontal';
+		const actual = screen.getByRole('button').children[1].children[0];
+
+		expect(actual).toHaveClass(expected);
+	});
+
+	test('should set "data-webos-voice-disabled" to IconButton', () => {
+		render(<LabeledIconButton data-webos-voice-disabled>star</LabeledIconButton>);
+
+		const expected = 'data-webos-voice-disabled';
+		const actual = screen.getByRole('button');
+
+		expect(actual).toHaveAttribute(expected);
+	});
+
+	test('should set "data-webos-voice-group-label" to IconButton', () => {
+		const voiceGroupLabel = 'voice group label';
+		render(<LabeledIconButton data-webos-voice-group-label={voiceGroupLabel}>star</LabeledIconButton>);
+
+		const expected = voiceGroupLabel;
+		const actual = screen.getByRole('button');
+
+		expect(actual).toHaveAttribute('data-webos-voice-group-label', expected);
+	});
+
+	test('should set "data-webos-voice-label" to IconButton', () => {
+		const voiceLabel = 'voice label';
+		render(<LabeledIconButton data-webos-voice-label={voiceLabel}>star</LabeledIconButton>);
+
+		const expected = voiceLabel;
+		const actual = screen.getByRole('button');
+
+		expect(actual).toHaveAttribute('data-webos-voice-label', expected);
+	});
+
+	test('should set "data-webos-voice-intent" to IconButton', () => {
+		const voiceIntent = 'Select PlayContent';
+		render(<LabeledIconButton data-webos-voice-intent={voiceIntent}>star</LabeledIconButton>);
+
+		const expected = voiceIntent;
+		const actual = screen.getByRole('button');
+
+		expect(actual).toHaveAttribute('data-webos-voice-intent', expected);
+	});
 });
