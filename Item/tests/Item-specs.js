109dcdc22e08328a99cebd358257a8d025cbdea0
Stanca Pop <stanca.pop@lgepartner.com>
2021-07-29 14:09:09 +0300

    review fixes

--- Item/tests/Item-specs.js
+++ Item/tests/Item-specs.js
@@ -7,12 +7,13 @@ describe('Item Specs', () => {
 	test('should support adding text as a child', () => {
 		const expected = 'Hello Item';
 		render(<ItemBase>{expected}</ItemBase>);
+
 		const actual = screen.getByText('Hello Item');
 
 		expect(actual).toHaveTextContent(expected);
 	});
 
-	test('should apply `inline` class when inline', function () {
+	test('should apply `inline` class when inline', () => {
 		render(<ItemBase inline>Hello Item</ItemBase>);
 
 		const expected = 'inline';
@@ -21,7 +22,7 @@ describe('Item Specs', () => {
 		expect(actual).toHaveClass(expected);
 	});
 
-	test('should have `aria-disabled=true` attribute when disabled', function () {
+	test('should have `aria-disabled=true` attribute when disabled', () => {
 		render(<ItemBase disabled>Hello Item</ItemBase>);
 
 		const expected = 'true';
--- SelectableItem/tests/SelectableItem-specs.js
+++ SelectableItem/tests/SelectableItem-specs.js
@@ -21,5 +21,4 @@ describe('SelectableItem Specs', () => {
 
 		expect(actual).toHaveClass(expected);
 	});
-
 });
