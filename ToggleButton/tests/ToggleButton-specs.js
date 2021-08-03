d05cbdabcd7bdb1b19ad053ab3513c9e4cfa5f78
Stanca Pop <stanca.pop@lgepartner.com>
2021-08-03 21:55:11 +0300

    review fixes

--- ToggleButton/tests/ToggleButton-specs.js
+++ ToggleButton/tests/ToggleButton-specs.js
@@ -4,85 +4,46 @@ import {render, screen} from '@testing-library/react';
 import ToggleButton from '../ToggleButton';
 
 describe('ToggleButton', () => {
-
 	const toggleOnLabel = 'It\'s on!';
 	const toggleOffLabel = 'It\'s off!';
 	const textChild = 'Toggle Me';
 
-	test(
-		'should use \'toggleOffLabel\' if toggled off and label provided',
-		() => {
-			render(
-				<ToggleButton toggleOffLabel={toggleOffLabel}>
-					{textChild}
-				</ToggleButton>
-			);
-			const button = screen.getByRole('button');
+	test('should use \'toggleOffLabel\' if toggled off and label provided', () => {
+		render(<ToggleButton toggleOffLabel={toggleOffLabel}>{textChild}</ToggleButton>);
+		const button = screen.getByRole('button');
 
-			const expected = toggleOffLabel;
-			const actual = button.children[2].children[0].children[0];
+		const expected = toggleOffLabel;
+		const actual = button.children[2].children[0].children[0];
 
-			expect(actual).toHaveTextContent(expected);
-		}
-	);
+		expect(actual).toHaveTextContent(expected);
+	});
 
-	test(
-		'should use \'toggleOnLabel\' if toggled on and label provided',
-		() => {
-			render(
-				<ToggleButton toggleOnLabel={toggleOnLabel} selected>
-					{textChild}
-				</ToggleButton>
-			);
-			const button = screen.getByRole('button');
+	test('should use \'toggleOnLabel\' if toggled on and label provided', () => {
+		render(<ToggleButton toggleOnLabel={toggleOnLabel} selected>{textChild}</ToggleButton>);
+		const button = screen.getByRole('button');
 
-			const expected = toggleOnLabel;
-			const actual = button.children[2].children[0].children[0];
+		const expected = toggleOnLabel;
+		const actual = button.children[2].children[0].children[0];
 
-			expect(actual).toHaveTextContent(expected);
-		}
-	);
+		expect(actual).toHaveTextContent(expected);
+	});
 
-	test(
-		'should use child node for label when \'toggleOffLabel\' is missing',
-		() => {
-			render(
-				<ToggleButton toggleOnLabel={toggleOnLabel}>
-					{textChild}
-				</ToggleButton>
-			);
-			const button = screen.getByRole('button');
+	test('should use child node for label when \'toggleOffLabel\' is missing', () => {
+		render(<ToggleButton toggleOnLabel={toggleOnLabel}>{textChild}</ToggleButton>);
+		const button = screen.getByRole('button');
 
-			const expected = textChild;
-			const actual = button.children[2].children[0].children[0];
+		expect(button).toHaveTextContent(textChild);
+	});
 
-			expect(actual).toHaveTextContent(expected);
-		}
-	);
+	test('should use child node for label when \'toggleOnLabel\' is missing', () => {
+		render(<ToggleButton toggleOffLabel={toggleOffLabel} selected>{textChild}</ToggleButton>);
+		const button = screen.getByRole('button');
 
-	test(
-		'should use child node for label when \'toggleOnLabel\' is missing',
-		() => {
-			render(
-				<ToggleButton toggleOffLabel={toggleOffLabel} selected>
-					{textChild}
-				</ToggleButton>
-			);
-			const button = screen.getByRole('button');
-
-			const expected = textChild;
-			const actual = button.children[2].children[0].children[0];
-
-			expect(actual).toHaveTextContent(expected);
-		}
-	);
+		expect(button).toHaveTextContent(textChild);
+	});
 
 	test('should set "aria-pressed" to the value of "selected"', () => {
-		render(
-			<ToggleButton toggleOffLabel={toggleOffLabel} selected={false}>
-				{textChild}
-			</ToggleButton>
-		);
+		render(<ToggleButton toggleOffLabel={toggleOffLabel} selected={false}>{textChild}</ToggleButton>);
 
 		const expected = 'false';
 		const actual = screen.getByRole('button');
