07a93b5e924c015e9ac4d2b5794087ed2a31686d
alexandrumorariu <84004128+alexandrumorariu@users.noreply.github.com>
2021-08-03 14:28:11 +0300

    Migrated unit tests from Enzyme to Testing-Library. (#76)

--- IconButton/tests/IconButton-specs.js
+++ IconButton/tests/IconButton-specs.js
@@ -1,34 +1,26 @@
-import {mount} from 'enzyme';
+import '@testing-library/jest-dom';
+import {render, screen} from '@testing-library/react';
+
 import IconButton from '../IconButton';
 
 describe('IconButton Specs', () => {
+	test('should apply same `size` prop to both <Icon> and <Button> children', () => {
+		render(<IconButton size="small">star</IconButton>);
+		const button = screen.getByRole('button');
+		const icon = button.lastElementChild.firstElementChild;
 
-	test(
-		'should apply same \'size\' prop to both <Icon> and <Button> children',
-		() => {
-			const iconButton = mount(
-				<IconButton size="small">star</IconButton>
-			);
-			const icon = iconButton.find('Icon');
-			const button = iconButton.find('Button');
-			const expected = true;
-			const actual = (icon.prop('size') === button.prop('size'));
+		const expected = 'small';
 
-			expect(actual).toBe(expected);
-		}
-	);
+		expect(button).toHaveClass(expected);
+		expect(icon).toHaveClass(expected);
+	});
 
-	test(
-		'should always maintain minWidth=false for its <Button> child',
-		() => {
-			const iconButton = mount(
-				<IconButton minWidth>star</IconButton>
-			);
-			const button = iconButton.find('Button');
-			const expected = false;
-			const actual = (button.prop('minWidth'));
+	test('should always maintain minWidth=false for its <Button> child', () => {
+		render(<IconButton minWidth>star</IconButton>);
+		const button = screen.getByRole('button');
 
-			expect(actual).toBe(expected);
-		}
-	);
+		const notExpected = 'minWidth';
+
+		expect(button).not.toHaveClass(notExpected);
+	});
 });
