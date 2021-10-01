888bda2c456e6b8b053943aff5c47659fb505fbf
dongsu.won <dongsu.won@lgepartner.com>
2021-10-01 09:49:48 +0900

    * Add native virtuallist sample.
        Enact-DCO-1.0-Signed-off-by: Dongsu Won (dongsu.won@lgepartner.com)

---
+++ samples/qa-virtuallistnative/src/index.js
@@ -0,0 +1,20 @@
+import {Provider} from 'react-redux';
+import {render} from 'react-dom';
+
+import App from './App';
+import configureStore from './store';
+
+const store = configureStore();
+
+const appElement = (
+	<Provider store={store}>
+		<App />
+	</Provider>
+);
+
+// In a browser environment, render the app to the document.
+if (typeof window !== 'undefined') {
+	render(appElement, document.getElementById('root'));
+}
+
+export default appElement;
