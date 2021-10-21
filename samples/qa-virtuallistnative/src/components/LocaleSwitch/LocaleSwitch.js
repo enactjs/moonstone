8efff539304b3f0a9212d90226f58214985c66a8
dongsuwon <61816767+dongsuwon@users.noreply.github.com>
2021-10-21 16:16:53 +0900

    WRN-6418: Need to add native samples. (#87)
    * WRN-6418: VirtualList: Add support for snapToCenter (#963)
    WRN-6418: Need to add native samples.
    
    * Add native scroller sample.
    
    Enact-DCO-1.0-Signed-off-by: Dongsu Won (dongsu.won@lgepartner.com)
    
    * * Fix Readme and appinfo files
    
    Enact-DCO-1.0-Signed-off-by: Dongsu Won (dongsu.won@lgepartner.com)
    
    * * Add native virtuallist sample.
    
        Enact-DCO-1.0-Signed-off-by: Dongsu Won (dongsu.won@lgepartner.com)
    
    * * Add native virtualgridlist sample.
    
    Enact-DCO-1.0-Signed-off-by: Dongsu Won (dongsu.won@lgepartner.com)
    
    * * Change folder name for virtualgridlist-preserve-focus sample
    
    Enact-DCO-1.0-Signed-off-by: Dongsu Won (dongsu.won@lgepartner.com)
    
    * * Suggested chagnes.
    
    Enact-DCO-1.0-Signed-off-by: Dongsu Won (dongsu.won@lgepartner.com)

---
+++ samples/qa-virtuallistnative/src/components/LocaleSwitch/LocaleSwitch.js
@@ -0,0 +1,16 @@
+import {useI18nContext} from '@enact/i18n/I18nDecorator';
+import {useCallback} from 'react';
+import ToggleButton from '@enact/moonstone/ToggleButton';
+
+const LocaleSwitch = (props) => {
+	const {rtl, updateLocale} = useI18nContext();
+	const onClick = useCallback(() => {
+		updateLocale(!rtl ? 'ar-SA' : 'en-US');
+	}, [rtl, updateLocale]);
+
+	return (
+		<ToggleButton onClick={onClick} {...props}>RTL</ToggleButton>
+	);
+};
+
+export default LocaleSwitch;
