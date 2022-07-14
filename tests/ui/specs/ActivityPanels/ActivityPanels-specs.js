d7edbc673ad455bb7a21f539f0ccf471d86a0e16
Daniel Stoian <daniel.stoian@lgepartner.com>
2022-07-14 10:50:15 +0300

    testing ui-tests

--- tests/ui/specs/ActivityPanels/ActivityPanels-specs.js
+++ tests/ui/specs/ActivityPanels/ActivityPanels-specs.js
@@ -12,7 +12,7 @@ describe('ActivityPanels', function () {
 	});
 
 	it('should have breadcrumb on second panel', async function () {
-		await Page.waitTransitionEnd(5000, undefined, () => {
+		await Page.waitTransitionEnd(2000, undefined, () => {
 			Page.button1.click();
 		}, []);
 
@@ -23,7 +23,7 @@ describe('ActivityPanels', function () {
 
 	describe('Transition', function () {
 		it('should move from first panel to the second', async function () {
-			await Page.waitTransitionEnd(5000, undefined, () => {
+			await Page.waitTransitionEnd(2000, undefined, () => {
 				Page.button1.click();
 			}, []);
 
@@ -31,16 +31,16 @@ describe('ActivityPanels', function () {
 		});
 
 		it('should navigate to Last Focused', async function () {
-			await Page.waitTransitionEnd(5000, undefined, () => {
+			await Page.waitTransitionEnd(2000, undefined, () => {
 				Page.item1.click();
 			}, []);
-			await Page.waitTransitionEnd(5000, undefined, () => {
+			await Page.waitTransitionEnd(2000, undefined, () => {
 				Page.item5.click();
 			}, []);
-			await Page.waitTransitionEnd(5000, undefined, () => {
+			await Page.waitTransitionEnd(2000, undefined, () => {
 				Page.button4.click();
 			}, []);
-			await Page.waitTransitionEnd(5000, undefined, () => {
+			await Page.waitTransitionEnd(2000, undefined, () => {
 				Page.item2.click();
 			}, []);
 
@@ -48,52 +48,72 @@ describe('ActivityPanels', function () {
 		});
 
 		it('should navigate back to the First panel from clicking on breadcrumb', async function () {
-			await Page.waitTransitionEnd(5000, undefined, async () => {
+			await Page.waitTransitionEnd(2000, 'one', async () => {
+				console.log("click1");
 				await Page.item1.click();
 			}, []);
-			await Page.waitTransitionEnd(5000, undefined, async () => {
+			await browser.pause(200);
+			console.log(await Page.panelTitle)
+			await Page.waitTransitionEnd(2000, 'two', async () => {
+				console.log("click2");
 				await Page.item5.click();
 			}, []);
-			await Page.waitTransitionEnd(5000, undefined, async () => {
+			await browser.pause(200);
+			console.log(await Page.panelTitle)
+			await Page.waitTransitionEnd(2000, 'three', async () => {
+				console.log("click3");
 				await Page.button4.click();
 			}, []);
-			await Page.waitTransitionEnd(5000, undefined, async () => {
+			await browser.pause(200);
+			console.log(await Page.panelTitle)
+			await Page.waitTransitionEnd(2000, 'four', async () => {
+				console.log("click4");
 				await Page.item2.click();
 			}, []);
-			await Page.waitTransitionEnd(5000, undefined, async () => {
+			await browser.pause(200);
+			console.log(await Page.panelTitle)
+			await Page.waitTransitionEnd(2000, 'five', async () => {
+				console.log("click5");
 				await Page.breadcrumbHeader.click();
 			}, []);
-			await Page.waitTransitionEnd(5000, undefined, async () => {
+			await browser.pause(200);
+			console.log(await Page.panelTitle)
+			await Page.waitTransitionEnd(2000, 'six', async () => {
+				console.log("click6");
 				await Page.breadcrumbHeader.click();
 			}, []);
-			await Page.waitTransitionEnd(5000, undefined, async () => {
+			await browser.pause(200);
+			console.log(await Page.panelTitle);
+			await Page.waitTransitionEnd(2000, 'seven', async () => {
 				await Page.breadcrumbHeader.click();
 			}, []);
-			await Page.waitTransitionEnd(5000, undefined, async () => {
+			await browser.pause(200);
+			console.log(await Page.panelTitle)
+			await Page.waitTransitionEnd(2000, 'eight', async () => {
 				await Page.breadcrumbHeader.click();
 			}, []);
-
-			await browser.pause(500);
+			await browser.pause(200)
+			console.log(await Page.panelTitle);
 			expect((await Page.panelTitle).toLowerCase()).to.equal('FIRST'.toLowerCase());
 		});
 
 		it('should navigate back to the Third panel from clicking on breadcrumb', async function () {
-			await Page.waitTransitionEnd(5000, 'one', () => {
+			await Page.waitTransitionEnd(2000, 'one', () => {
 				Page.item1.click();
 			}, []);
-			await Page.waitTransitionEnd(5000, 'two', () => {
+			await Page.waitTransitionEnd(2000, 'two', () => {
 				Page.item5.click();
 			}, []);
-			await Page.waitTransitionEnd(5000, 'three', () => {
+			await Page.waitTransitionEnd(2000, 'three', () => {
 				Page.breadcrumbHeader.click();
 			}, []);
-			await Page.waitTransitionEnd(5000, 'four', () => {
+			await Page.waitTransitionEnd(2000, 'four', () => {
 				Page.item8.click();
 			}, []);
-			await Page.waitTransitionEnd(5000, 'five', () => {
+			await Page.waitTransitionEnd(2000, 'five', () => {
 				Page.button4.click();
 			}, []);
-			await Page.waitTransitionEnd(5000, 'six', () => {
+			await Page.waitTransitionEnd(2000, 'six', () => {
 				Page.breadcrumbHeader.click();
 			}, []);
 
@@ -102,13 +122,13 @@ describe('ActivityPanels', function () {
 
 		it('should move from first panel to the third', async function () {
 			await Page.button1.moveTo();
-			await Page.waitTransitionEnd(5000, undefined, () => {
+			await Page.waitTransitionEnd(2000, undefined, () => {
 				Page.spotlightSelect();
 			}, []);
 
 			expect((await Page.panelTitle).toLowerCase()).to.equal('SECOND'.toLowerCase());
 			await Page.item8.moveTo();
-			await Page.waitTransitionEnd(5000, undefined, () => {
+			await Page.waitTransitionEnd(2000, undefined, () => {
 				Page.spotlightSelect();
 			}, []);
 
@@ -117,26 +137,26 @@ describe('ActivityPanels', function () {
 
 		it('should move to first panel from the third', async function () {
 			await Page.button1.moveTo();
-			await Page.waitTransitionEnd(5000, undefined, () => {
+			await Page.waitTransitionEnd(2000, undefined, () => {
 				Page.spotlightSelect();
 			}, []);
 
 			expect((await Page.panelTitle).toLowerCase()).to.equal('SECOND'.toLowerCase());
 			await Page.item8.moveTo();
-			await Page.waitTransitionEnd(5000, undefined, () => {
+			await Page.waitTransitionEnd(2000, undefined, () => {
 				Page.spotlightSelect();
 			}, []);
 
 			expect((await Page.panelTitle).toLowerCase()).to.equal('THIRD'.toLowerCase());
 			await Page.breadcrumbHeader.moveTo();
-			await Page.waitTransitionEnd(5000, undefined, () => {
+			await Page.waitTransitionEnd(2000, undefined, () => {
 				Page.spotlightSelect();
 			}, []);
 
 			expect((await Page.panelTitle).toLowerCase()).to.equal('SECOND'.toLowerCase());
 			await Page.item8.moveTo();
 			await Page.breadcrumbHeader.moveTo();
-			await Page.waitTransitionEnd(5000, undefined, () => {
+			await Page.waitTransitionEnd(2000, undefined, () => {
 				Page.spotlightSelect();
 			}, []);
 
@@ -144,11 +164,11 @@ describe('ActivityPanels', function () {
 		});
 
 		it('should transition back to First panel with back key', async function () {
-			await Page.waitTransitionEnd(5000, undefined, () => {
+			await Page.waitTransitionEnd(2000, undefined, () => {
 				Page.button1.click();
 			}, []);
 			expect((await Page.panelTitle).toLowerCase()).to.equal('SECOND'.toLowerCase());
-			await Page.waitTransitionEnd(5000, undefined, () => {
+			await Page.waitTransitionEnd(2000, undefined, () => {
 				Page.backKey();
 			}, []);
 
@@ -164,10 +184,10 @@ describe('ActivityPanels', function () {
 		describe('pointer', function () {
 			// The ESC button (Back Key) does _not_ unset the pointer mode and does _not_ focus [ENYO-5865] [ENYO-5882]
 			it('should Not spot last focused item when transitioning back', async function () {
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.item2.click();
 				}, []);
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.backKey();
 				}, []);
 
@@ -176,11 +196,11 @@ describe('ActivityPanels', function () {
 
 			// The ESC button (Back Key) does _not_ unset the pointer mode and does _not_ focus [ENYO-5865] [ENYO-5882]
 			it('should Not spot last focused item when transitioning back after moving pointer', async function () {
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.item2.click();
 				}, []);
 				Page.item8.moveTo();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.backKey();
 				}, []);
 
@@ -191,7 +211,7 @@ describe('ActivityPanels', function () {
 
 		describe('5way', function () {
 			it('should spot first item on second panel', async function () {
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
 
@@ -200,11 +220,11 @@ describe('ActivityPanels', function () {
 
 			it('should spot last focused item when transitioning back using back key', async function () {
 				Page.spotlightDown();
-				await Page.waitTransitionEnd(5000, 'panel open', () => {
+				await Page.waitTransitionEnd(2000, 'panel open', () => {
 					Page.spotlightSelect();
 				}, []);
 				expect(await Page.item5.isFocused()).to.be.true();
-				await Page.waitTransitionEnd(5000, 'panel back', () => {
+				await Page.waitTransitionEnd(2000, 'panel back', () => {
 					Page.backKey();
 				}, []);
 
@@ -216,12 +236,12 @@ describe('ActivityPanels', function () {
 			it('should spot last focused item when transitioning back from Third panel', async function () {
 				Page.spotlightDown();
 				Page.spotlightDown();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
 
 				expect(await Page.item5.isFocused(), 'Item 5 focus').to.be.true();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
 
@@ -231,7 +251,7 @@ describe('ActivityPanels', function () {
 				Page.spotlightLeft();
 				Page.spotlightLeft();
 				expect(await Page.breadcrumb.isFocused(), 'Breadcrumb focus').to.be.true();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
 
@@ -240,22 +260,22 @@ describe('ActivityPanels', function () {
 
 			it('should spot last focused item in first panel when transitioning after deep navigation', async function () {
 				Page.spotlightDown();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
 
 				Page.spotlightDown();
 				expect(await Page.item6.isFocused(), 'Item 6 focus').to.be.true();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
 
 				expect(await Page.button3.isFocused(), 'Button 3 focus').to.be.true();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.backKey();
 				}, []);
 				expect(await Page.item6.isFocused(), 'Item 6 refocus').to.be.true();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.backKey();
 				}, []);
 				expect(await Page.item2.isFocused(), 'Item 2 refocus').to.be.true();
@@ -267,7 +287,7 @@ describe('ActivityPanels', function () {
 				await Page.spotlightDown();
 				await Page.spotlightDown();
 				await Page.spotlightDown();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
 
@@ -286,18 +306,18 @@ describe('ActivityPanels', function () {
 			});
 
 			it('should spot the seventh item on last panel', async function () {
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
 				await Page.spotlightRight();
 				await Page.spotlightRight();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
 
@@ -313,13 +333,13 @@ describe('ActivityPanels', function () {
 			it('should spot third item on first panel', async function () {
 				await Page.spotlightDown();
 				await Page.spotlightDown();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
 
 				expect(await Page.item5.isFocused()).to.be.true();
 				await Page.spotlightLeft();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
 
@@ -330,18 +350,18 @@ describe('ActivityPanels', function () {
 		describe('5way and pointer', function () {
 			it('should not spot in None panel', async function () {
 				await Page.button1.moveTo();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
 
 				expect((await Page.panelTitle).toLowerCase()).to.equal('SECOND'.toLowerCase());
 				await Page.item8.moveTo();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
 
 				expect(await Page.button3.isFocused()).to.be.true();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
 
@@ -350,25 +370,25 @@ describe('ActivityPanels', function () {
 
 			it('should spot default item in Default panel', async function () {
 				await Page.button1.moveTo();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
 
 				expect((await Page.panelTitle).toLowerCase()).to.equal('SECOND'.toLowerCase());
 				await Page.item8.moveTo();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
 
 				expect((await Page.panelTitle).toLowerCase()).to.equal('THIRD'.toLowerCase());
 				await Page.button4.moveTo();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
 
 				expect((await Page.panelTitle).toLowerCase()).to.equal('NONE'.toLowerCase());
 				await Page.button1.moveTo();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
 
@@ -377,18 +397,18 @@ describe('ActivityPanels', function () {
 
 			it('should re-spot last focused in last focused panel', async function () {
 				await Page.button1.moveTo();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
 
 				expect(await Page.item5.isFocused(), 'item 5 focus 1').to.be.true();
 				await Page.item8.moveTo();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
 
 				expect(await Page.button3.isFocused(), 'button 3 focus').to.be.true();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
 
@@ -396,18 +416,18 @@ describe('ActivityPanels', function () {
 				await Page.spotlightDown();
 				expect(await Page.breadcrumb.isFocused(), 'breadcrumb focus').to.be.true();
 				await Page.button1.moveTo();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
 
 				expect(await Page.item5.isFocused(), 'item 5 focus 2').to.be.true();
 				// Focus to item 6 so it can be last-focused item when returning
 				await Page.spotlightDown();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.backKey();
 				}, []);
 				await Page.spotlightDown();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
 
@@ -416,21 +436,21 @@ describe('ActivityPanels', function () {
 
 			it('should spot last focused item when transitioning back with Back key, deep navigation', async function () {
 				await Page.item3.moveTo();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
 
 				expect(await Page.item5.isFocused()).to.be.true();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.spotlightSelect();
 				}, []);
 
 				expect(await Page.button3.isFocused()).to.be.true();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.backKey();
 				}, []);
 				expect(await Page.item5.isFocused()).to.be.true();
-				await Page.waitTransitionEnd(5000, undefined, () => {
+				await Page.waitTransitionEnd(2000, undefined, () => {
 					Page.backKey();
 				}, []);
 
