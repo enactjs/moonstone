61b288e9e1507344c7e6dbdd754d1fd71790f9ab
Daniel Stoian <daniel.stoian@lgepartner.com>
2022-07-27 15:37:34 +0300

    Migrated qa-samplers for MArquee.js, Tooltip and Scroller and ToggleButton

--- samples/sampler/stories/qa/ToggleButton.js
+++ samples/sampler/stories/qa/ToggleButton.js
@@ -1,47 +1,64 @@
-// import {action} from '@enact/storybook-utils/addons/actions';
-// import {boolean, select, text} from '@enact/storybook-utils/addons/controls';
-// import {mergeComponentMetadata} from '@enact/storybook-utils';
-// import UiButton, {ButtonBase as UIButtonBase} from '@enact/ui/Button';
-// import {storiesOf} from '@storybook/react';
-//
-// import Button, {ButtonBase} from '@enact/moonstone/Button';
-// import ToggleButton from '@enact/moonstone/ToggleButton';
-//
-// ToggleButton.displayName = 'ToggleButton';
-// const Config = mergeComponentMetadata('ToggleButton', UIButtonBase, UiButton, ButtonBase, Button, ToggleButton);
-//
-// // Set up some defaults for info and knobs
-// const prop = {
-// 	backgroundOpacity: ['', 'translucent', 'lightTranslucent', 'transparent'],
-// 	tallText:{'ฟิ้ ไั ஒ து': 'ฟิ้ ไั ஒ து', 'ÁÉÍÓÚÑÜ': 'ÁÉÍÓÚÑÜ', 'Bản văn': 'Bản văn'}
-// };
-//
-// storiesOf('ToggleButton', module)
-// 	.add(
-// 		'with long text',
-// 		() => (
-// 			<ToggleButton
-// 				onClick={action('onClick')}
-// 				onToggle={action('onToggle')}
-// 				backgroundOpacity={select('backgroundOpacity', prop.backgroundOpacity, Config)}
-// 				disabled={boolean('disabled', Config)}
-// 				size={select('size', ['small', 'large'], Config)}
-// 				toggleOnLabel={text('toggleOnLabel', Config, 'I am an extremely and particularly very long button with label On')}
-// 				toggleOffLabel={text('toggleOffLabel', Config, 'I am an extremely and particularly very long button with label Off')}
-// 			/>
-// 		)
-// 	)
-// 	.add(
-// 		'with tall characters',
-// 		() => (
-// 			<ToggleButton
-// 				onClick={action('onClick')}
-// 				onToggle={action('onToggle')}
-// 				backgroundOpacity={select('backgroundOpacity', prop.backgroundOpacity, Config)}
-// 				disabled={boolean('disabled', Config)}
-// 				size={select('size', ['small', 'large'], Config)}
-// 				toggleOnLabel={select('toggleOnLabel', prop.tallText, Config, 'ฟิ้ ไั ஒ து')}
-// 				toggleOffLabel={select('toggleOffLabel', prop.tallText, Config, 'ฟิ้ ไั ஒ து')}
-// 			/>
-// 		)
-// 	);
+import Button, {ButtonBase} from '@enact/moonstone/Button';
+import ToggleButton from '@enact/moonstone/ToggleButton';
+import {mergeComponentMetadata} from '@enact/storybook-utils';
+import {action} from '@enact/storybook-utils/addons/actions';
+import {boolean, select, text} from '@enact/storybook-utils/addons/controls';
+import UiButton, {ButtonBase as UIButtonBase} from '@enact/ui/Button';
+
+ToggleButton.displayName = 'ToggleButton';
+const Config = mergeComponentMetadata('ToggleButton', UIButtonBase, UiButton, ButtonBase, Button, ToggleButton);
+
+// Set up some defaults for info and knobs
+const prop = {
+	backgroundOpacity: ['', 'translucent', 'lightTranslucent', 'transparent'],
+	tallText:{'ฟิ้ ไั ஒ து': 'ฟิ้ ไั ஒ து', 'ÁÉÍÓÚÑÜ': 'ÁÉÍÓÚÑÜ', 'Bản văn': 'Bản văn'}
+};
+
+export default {
+	title: 'Moonstone/ToggleButton',
+	component: 'ToggleButton'
+};
+
+export const WithLongText = (args) => {
+	return (
+		<ToggleButton
+			onClick={action('onClick')}
+			onToggle={action('onToggle')}
+			backgroundOpacity={args['backgroundOpacity']}
+			disabled={args['disabled']}
+			size={args['size']}
+			toggleOnLabel={args['toggleOnLabel']}
+			toggleOffLabel={args['toggleOffLabel']}
+		/>
+	);
+};
+
+select('backgroundOpacity', WithLongText, prop.backgroundOpacity, Config);
+boolean('disabled', WithLongText, Config);
+select('size', WithLongText, ['small', 'large'], Config);
+text('toggleOnLabel', WithLongText, Config, 'I am an extremely and particularly very long button with label On');
+text('toggleOffLabel', WithLongText, Config, 'I am an extremely and particularly very long button with label Off');
+
+WithLongText.storyName = 'with long text';
+
+export const WithTallText = (args) => {
+	return (
+		<ToggleButton
+			onClick={action('onClick')}
+			onToggle={action('onToggle')}
+			backgroundOpacity={args['backgroundOpacity']}
+			disabled={args['disabled']}
+			size={args['size']}
+			toggleOnLabel={args['toggleOnLabel']}
+			toggleOffLabel={args['toggleOffLabel']}
+		/>
+	);
+};
+
+select('backgroundOpacity', WithTallText, prop.backgroundOpacity, Config);
+boolean('disabled', WithTallText, Config);
+select('size', WithTallText, ['small', 'large'], Config);
+text('toggleOnLabel', WithTallText, Config, 'ฟิ้ ไั ஒ து');
+text('toggleOffLabel', WithTallText, Config, 'ฟิ้ ไั ஒ து');
+
+WithTallText.storyName = 'with tall characters';
