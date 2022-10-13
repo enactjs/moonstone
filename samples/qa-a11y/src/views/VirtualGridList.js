31203d8e2d2538b7dafc496bedf7e5855096bf7e
Andrei Tirla <andrei.tirla@lgepartner.com>
2022-10-13 13:37:07 +0300

    Added local svg for qa-a11y/VirtualGridList

--- samples/qa-a11y/src/views/VirtualGridList.js
+++ samples/qa-a11y/src/views/VirtualGridList.js
@@ -9,7 +9,13 @@ import {useCallback, useState} from 'react';
 import css from './VirtualGridList.module.less';
 
 const
-	items = [],
+	items = [];
+
+const svgGenerator = (width, height, bgColor, textColor, customText) => (
+	`data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 ${width} ${height}' width='${width}' height='${height}'%3E` +
+	`%3Crect width='${width}' height='${height}' fill='%23${bgColor}'%3E%3C/rect%3E` +
+	`%3Ctext x='50%25' y='50%25' dominant-baseline='middle' text-anchor='middle' font-size='36px' fill='%23${textColor}'%3E${customText}%3C/text%3E%3C/svg%3E`
+	),
 	// eslint-disable-next-line enact/prop-types
 	renderItem = ({index, ...rest}) => {
 		const
@@ -35,7 +41,7 @@ for (let i = 0; i < 100; i++) {
 		text = `Item ${count}`,
 		subText = `SubItem ${count}`,
 		color = Math.floor((Math.random() * (0x1000000 - 0x101010)) + 0x101010).toString(16),
-		source = `http://via.placeholder.com/300x300/${color}/ffffff/png?text=Image+${i}`;
+		source = svgGenerator(600, 600, color, 'ffffff', `Image ${i}`);
 
 	items.push({text, subText, source});
 }
