8e3f981694ccc5d0eb53717c638c08694be5ec2a
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2024-01-23 16:44:31 +0200

    revert UiRadioDecorator.js and fix UiRadioControllerDecorator

--- UiRadioDecorator/UiRadioDecorator.js
+++ UiRadioDecorator/UiRadioDecorator.js
@@ -24,7 +24,7 @@ const defaultConfig = {
 	 *
 	 * @type {String}
 	 * @default null
-	 * @memberof moonstone/UiRadioDecorator.defaultConfig
+	 * @memberof moonstone/UiRadioDecorator.RadioDecorator.defaultConfig
 	 */
 	activate: null,
 
@@ -33,7 +33,7 @@ const defaultConfig = {
 	 *
 	 * @type {String}
 	 * @default null
-	 * @memberof moonstone/UiRadioDecorator.defaultConfig
+	 * @memberof moonstone/UiRadioDecorator.RadioDecorator.defaultConfig
 	 */
 	deactivate: null,
 
@@ -42,7 +42,7 @@ const defaultConfig = {
 	 *
 	 * @type {String}
 	 * @default 'active'
-	 * @memberof moonstone/UiRadioDecorator.defaultConfig
+	 * @memberof moonstone/UiRadioDecorator.RadioDecorator.defaultConfig
 	 */
 	prop: 'active'
 };
